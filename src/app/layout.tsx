72e101e801a37c8746cdb7a650942e67bdd069dd
hux <huxley.millard@gmail.com>
2025-05-03 23:03:11 +0100

    refactor: lets remove that uneeded css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { SpotifyBar, Menu } from "./components/index";
 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://huxely-is-me.vercel.app/"),
@@ -40,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <main className="page">
           <SpotifyBar />
           <div className="wrapper">
